9d431462188e581b6afbf9844ee7a76e07286881
Валентин bc2025-2 <stelspro228@gmail.com>
2025-09-22 21:46:16 +0300

    Complete all parts of Laboratory Work #2

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const { URL } = require('url');
 
-const date = "202408";
+const date = "20240801";
 const time_period = "m";
 
-const base = "https://bank.gov.ua/NBUStatService/v1/statdirectory/FinResultsOfBanks.json";
+const url = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/banksincexp");
 
-const url = new URL(base);
 url.searchParams.append("date", date);
 url.searchParams.append("period", time_period);
+url.searchParams.append("json", "");
 
 console.log(url.toString());
